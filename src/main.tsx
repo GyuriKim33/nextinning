43ede4d8fdafc749ed3f97d88a0c88849d67dce4
Gyuri Kim <gyurikim@Gyuriui-MacBookAir.local>
2026-07-21 20:47:43 +0900

    Update Landing

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import App from './App'
 import './styles/variables.css'
 import './styles/global.css'
 
+if (/Windows/i.test(navigator.userAgent)) {
+  document.documentElement.classList.add('os-windows')
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
